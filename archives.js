910220bab6099fcead31f8e9782916aea05dfea9
ludo046 <ludomoissinac@icloud.com>
2024-01-15 20:45:36 +0100

    fisrt-version

---
+++ archives.js
@@ -0,0 +1,140 @@
+const year = [
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+  {video : 'https://www.youtube.com/embed/jsin-3UoWaE?si=iMjQ9IcUXFju0qv2', image : ['images/archives/2016/1.jpg','images/archives/2016/2.jpg']},
+]
+
+
+const startDate = new Date("2012-01-01");
+const endDate = new Date("2023-01-01");
+const totalYears = 11;
+const pixelsPerYear = 50;
+const totalWidth = 80 + "px";
+
+const timeline = document.getElementById("timeline");
+timeline.style.width = totalWidth + "px";
+for (let i = 0; i <= totalYears; i++) {
+  const event = document.createElement("div");
+  const eventYear = document.createElement("p");
+  event.classList.add("event");
+  event.appendChild(eventYear);
+  eventYear.classList.add("years");
+
+  // CALCUL DE LA POSITION EN FONCTION DE L'ANNÉÉ ACTUELLE
+  const currentDate = new Date(startDate);
+  currentDate.setFullYear(startDate.getFullYear() + i);
+  eventYear.innerText = [currentDate][0].getFullYear([i]);
+  eventYear.setAttribute("id", [currentDate][0].getFullYear([i]));
+
+  // CALCUL DE LA POSITION EN POURCENTAGE
+  const positionPercentage =
+    ((currentDate - startDate) / (endDate - startDate)) * 95;
+
+  // POSITIONNEMENT DE L'EVENEMENT SUR LA TIMELINE
+  event.style.top = positionPercentage + "%";
+
+  timeline.appendChild(event);
+
+  //RECUPETATION DE L'ID DE L'ANNEE
+  event.addEventListener("click", function () {
+    let id = eventYear.getAttribute("id");
+    changeYear(id);
+  });
+}
+
+function changeYear(year) {
+  // Logique de changement d'année ici
+  console.log(`Changement vers l'année ${year}`);
+  const section = 'year'+year;
+  console.log(section);
+  scrollToSection(section);
+}
+
+function createYearsCard(){
+    allCards = document.querySelector('.all-cards')
+    for(let i = 0; i <= totalYears; i++){
+        const yearCard = document.createElement('div')
+        yearCard.classList.add('year-card')
+        yearCard.setAttribute('id', 'year'+(2012+i))
+        const currentYear = document.createElement('h2')
+        currentYear.innerText = 2012+i
+        const yearContent = document.createElement('div')
+        yearContent.classList.add('year-content')
+        const teaser = document.createElement('iframe')
+        teaser.setAttribute('src', year[i].video)
+        if(window.innerWidth > 740){
+          teaser.style.height = '400px'
+        } else {
+          teaser.style.height = "40%"
+        }
+        teaser.style.width = "calc(100% - 50px)"
+        const caroussel = document.createElement('div')
+        caroussel.classList.add('carousel')
+        caroussel.setAttribute('id', 'carousel'+ (2012+i))
+        for(let j = 0; j < year[i].image.length; j++){
+          const slide = document.createElement('div')
+          slide.classList.add('slide')
+          const image = document.createElement('img')
+          image.src = year[i].image[j]
+          slide.appendChild(image)
+          caroussel.appendChild(slide)
+        }  
+
+        yearCard.appendChild(currentYear)
+        yearContent.appendChild(teaser)
+        yearContent.appendChild(caroussel)
+        yearCard.appendChild(yearContent)
+        allCards.appendChild(yearCard)
+
+
+    }
+}
+createYearsCard()
+
+  const carousels = document.querySelectorAll(".carousel");
+carousels.forEach((carousel) => {
+  let slides = carousel.querySelectorAll(".slide");
+  let currentSlide = 0;
+  const totalSlides = slides.length;
+
+  function showSlide(n) {
+    setTimeout(() => {
+      slides[currentSlide].style.display = "none";
+      currentSlide = (n + totalSlides) % totalSlides;
+      slides[currentSlide].style.display = "inline-block";
+    }, 500);
+  }
+
+  function nextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function previousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  // Show the initial slide
+  showSlide(currentSlide);
+
+  // Optionally, you can add interval for automatic sliding
+  setInterval(() => nextSlide(carousel), 2000); // Change slide every 3 seconds
+});
+
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  console.log(section);
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,61 @@
+const burgerBtn = document.querySelector('.burger-btn');
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar')
+
+window.onload = function(){
+    if(window.innerWidth > 740){
+        menu.style.height = 0;
+        menu.style.opacity = 0;
+    }
+}
+
+burgerBtn.addEventListener('click', function(){
+    if(menu.style.height == "0px"){
+        menu.style.height = "300px";
+        navbar.style.boxShadow = "none";
+        menu.style.opacity = 1;
+      } else {
+        menu.style.height = 0;
+        menu.style.opacity = 0;
+      }
+})
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  document.querySelector("#banner").style.backgroundPositionY =
+    offset * 0.2 + "px";
+  document.querySelector("#partenatiat").style.backgroundPositionY =
+    (offset - 900) * 0.2 + "px";
+});
+
+function createTroupeCard(){
+  const allCards = document.querySelector('.all-cards')
+  const troupes = [
+    {name : 'drakonia', img : 'images/au-programme/drakonia.jpeg', description : 'Déja venu au Malzieu, Drakonia revient cette année avec un tout nouveau spectacle à couper le souffle et assurera le spectacle des vendredi et samedi soirs. Billeterie ouverte a partir du 15 mars 2024'},
+    {name : 'claymor clan', img : 'images/au-programme/claymor-clan.webp', description : 'Vient pour la première fois au Malzieu, et viendra animer nos rues avec leur musique écossaise celtique.'},
+    {name : 'jehan serien', img : 'images/au-programme/jehan-serien.jpeg', description : `Venu l'année dernière, mais n'ayant pas pu nous raconter toutes ses petites anecdotes il revient cette année pour finir de nous les conter, mais ... il ne sera pas seul !`},
+    {name : `théatre de l'alauda`, img : 'images/au-programme/alauda.jpeg', description : `Une première au Malzieu, avec leur spectacle "et vorago". Et non vous ne rêvez pas vous croiserez bien une saucisse géante au coin d'une rue cette année.`},
+    {name : `kervan`, img : 'images/au-programme/kervan.webp', description : `Venus il y a quelques année une seule journée vous ne vous souvenez peut-être pas d'eux et de leurs magnifiques déambulations nous les retrouverons cette année dans nos ruelles ainsi qu'en première partie du spectacle de feu des vendredi et samedi soirs avec un spectacle inédit !`},
+    {name : `les tanneurs de drac`, img : 'images/au-programme/taneur .jpeg', description : `Les incountournables du Malzieu nous reviennent après quelques années, avec leurs musiques entrainantes.`},
+    {name : `les crapiauds`, img : 'images/au-programme/crapiauds.jpeg', description : `Reviennent cette année encore, mais cette fois ci avec un tout nouveau spectacle plein de magie !
+    Du rêve, de la magie, un grain de folie, un sombre sarrasin, un fantôme, un sorcier. Un moment pour vivre en famille (ou pas), l'expérience étrange et incroyable de pratiques occultes sorties tout droit du moyen âge.`},
+    {name : `caminaire`, img : 'images/au-programme/caminaire.jpeg', description : `Nos amis les Caminaire reviennent une fois de plus, avec leur musique médiévale, leurs voix et des pieds pour marcher.`},
+    {name : `la chevauchee des lices`, img : 'images/au-programme/chevauché.jpeg', description : `Nouvelle au Malzieu, une animation pour enfants qui pourront faire une course de petits chevaux, en jouant les chevaliers.`},
+  ]
+  for( let i = 0; i < troupes.length; i++){
+    const card  = document.createElement('li')
+    card.classList.add('troupe-card')
+    const image = document.createElement('img')
+    image.src = troupes[i].img
+    const name = document.createElement('h2')
+    name.innerHTML = troupes[i].name
+    const description = document.createElement('p')
+    description.innerHTML = troupes[i].description
+    card.appendChild(image);
+    card.appendChild(name);
+    card.appendChild(description);
+    allCards.appendChild(card);
+  }
+}
+createTroupeCard();
+
